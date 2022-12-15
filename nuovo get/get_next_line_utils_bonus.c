f7814672070a6bedbb2888ab491793d35cc29afc
Michele Girardi <mgirardi@c2r6p6.42firenze.it>
2022-12-16 00:21:36 +0100

    errata corrige

---
+++ nuovo get/get_next_line_utils_bonus.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgirardi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 14:12:50 by mgirardi          #+#    #+#             */
+/*   Updated: 2022/11/10 14:14:00 by mgirardi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *) s;
+	while (*ptr != c)
+	{
+		if (*ptr == 0)
+			return (NULL);
+		ptr++;
+	}
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *c)
+{
+	size_t	i;
+
+	i = 0;
+	while (c[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*c;
+
+	c = s;
+	i = 0;
+	while (i < n)
+	{
+		c[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*i;
+	size_t	x;
+
+	if (nitems == 0 || size == 0)
+	{
+		nitems = 1;
+		size = 1;
+	}
+	x = nitems * size;
+	i = malloc(x);
+	if (i == NULL)
+		return (NULL);
+	else
+		ft_bzero(i, x);
+	return (i);
+}
